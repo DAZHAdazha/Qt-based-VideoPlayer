f2d53079fb9cfb8c01333552ce273bf1c1b76e52
DAZHA <47051206+DAZHAdazha@users.noreply.github.com>
2020-12-11 17:13:25 +0800

    version-1

---
+++ videowidget.h
@@ -0,0 +1,21 @@
+
+
+#ifndef VIDEOWIDGET_H
+#define VIDEOWIDGET_H
+
+#include <QVideoWidget>
+
+class VideoWidget : public QVideoWidget
+{
+    Q_OBJECT
+
+public:
+    VideoWidget(QWidget *parent = 0);
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+    void mouseDoubleClickEvent(QMouseEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+};
+
+#endif // VIDEOWIDGET_H
